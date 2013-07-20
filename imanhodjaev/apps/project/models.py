e318325b50bf62acd8cdfd64e9ac4a80be323dab
Sultan Imanhodjaev <sultan.imanhodjaev@gmail.com>
2013-07-20 20:29:22 +0600

    Minor changes

--- imanhodjaev/apps/project/models.py
+++ imanhodjaev/apps/project/models.py
@@ -24,6 +24,9 @@ class Post(AbstractDatetime):
     project = models.ForeignKey(Project, null=True, blank=True)
     author = models.ForeignKey(User)
 
+    images = models.ForeignKey(PostImage, null=True, blank=True)
+    files = models.ForeignKey(PostFile, null=True, blank=True)
+
 
 class PostImage(AbstractDatetime):
     title = models.CharField(max_length=255, default='Image')
